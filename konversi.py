7e24d4ce013fe95e476f1e7bb793b3f443da21b1
nuricovicyyanto <nuricovicyyanto@gmail.com>
2022-07-07 19:53:39 +0700

    Create konversi.py

---
+++ konversi.py
@@ -0,0 +1,19 @@
+# konversi tipe data
+
+# tipa data integer
+number = 10
+print(type(number))
+
+# tipe data diubah menjadi string dengan menggunakan fungsi str()
+convert = str(number)
+print(type(convert))
+
+print("=" * 20)  # tanda pemisah
+
+# tipe data string
+teks = "20"
+print(type(teks))
+
+# tipe data diubah menjadi integer dengan menggunakan fungsi int()
+convertTeks = int(teks)
+print(type(convertTeks))
